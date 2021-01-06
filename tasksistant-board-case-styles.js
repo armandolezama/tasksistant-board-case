e3067b2c438920d39255e72cd6bc2b88e2cd4620
ArmandoClivo <armando@clivobot.com>
2021-01-06 12:39:24 -0600

    chore(*):Added styles, functionality and dependencies

--- tasksistant-board-case-styles.js
+++ tasksistant-board-case-styles.js
@@ -4,4 +4,94 @@ export default css`
   :host {
     margin: 0;
   }
+
+  #main-container {
+    display: inline-flex;
+    flex-direction: row;
+  }
+
+  #screen-container {
+    width: 500px;
+    height: 500px;
+    border: solid;
+    overflow: scroll;
+  }
+
+  #control-container {
+    border: solid;
+    border-left: none;
+    height: 500px;
+  }
+  
+  #control-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  #up-section ,
+  #middle-section,
+  #bottom-section,
+  #circle-container,
+  #screen-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  #circle-container {
+    width: 120px;
+    height: 120px;
+    align-items: center;
+  }
+
+  #arrow-up {
+    width: 0; 
+    height: 0; 
+    border-left: 60px solid transparent;
+    border-right: 60px solid transparent;
+    
+    border-bottom: 60px solid #bbb;
+  }
+
+  #arrow-down {
+    width: 0; 
+    height: 0; 
+    border-left: 60px solid transparent;
+    border-right: 60px solid transparent;
+    
+    border-top: 60px solid #bbb;
+  }
+
+  #arrow-right {
+    width: 0; 
+    height: 0; 
+    border-top: 60px solid transparent;
+    border-bottom: 60px solid transparent;
+    
+    border-left: 60px solid #bbb;
+  }
+
+  #arrow-left {
+    width: 0; 
+    height: 0; 
+    border-top: 60px solid transparent;
+    border-bottom: 60px solid transparent; 
+    
+    border-right:60px solid #bbb; 
+  }
+
+  #circle {
+    height: 100px;
+    width: 100px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+  }
+
+  input, button, label {
+    display: inline-flex;
+  }
 `;
--- tasksistant-board-case.js
+++ tasksistant-board-case.js
@@ -11,6 +11,11 @@ export class TasksistantBoardCase extends LitElement {
     */
   constructor() {
     super();
+    this.board = {}
+    this.currentNode = {};
+    this.buttonMessage = '';
+    this.rowsNumber = 0;
+    this.columnsNumber = 0;
   }
 
   /**
@@ -18,6 +23,11 @@ export class TasksistantBoardCase extends LitElement {
     */
   static get properties() {
     return {
+      board: {type:Object},
+      currentNode: {type:Object},
+      bottonMessage: {type: String},
+      rowsNumber: {type: Number},
+      columnsNumber: {type: Number}
     };
   }
   
@@ -25,8 +35,62 @@ export class TasksistantBoardCase extends LitElement {
     return styles
   }
 
+  firstUpdated() {
+    this.board = this.shadowRoot.querySelector('tasksistant-board-component');
+    this.buttonMessage = 'Load';
+  }
+
+  loadReloadBoard() {
+    this.board.linkBoardSpace();
+    this.currentNode = this.board.currentNode;
+  }
+
+  setBoardSpace(e) {
+    const dimension = e.target.name;
+    if(dimension === 'rows'){
+      this.rowsNumber = e.target.value;
+    } else if (dimension === 'columns'){
+      this.columnsNumber = e.target.value;
+    }
+  }
+  
+  _moveCurrentNode(e){
+    const direction = e.target.getAttribute('value');
+    this.board.navigateFromCurrentNodeTo(direction);
+    this.currentNode = this.board.currentNode;
+  }
+
   render() {
-    return html``;
+    return html`
+    <div id="main-container">
+      <div id="screen-container">
+        <tasksistant-board-component .numberOfRows="${this.rowsNumber}" .numberOfColumns="${this.columnsNumber}"></tasksistant-board-component>
+      </div>
+      <div id="control-container">
+      <label for="rows">Number of rows</label>
+        <input id="rows" type="number" placeholder="Insert a number for rows" name="rows" value="0" @input="${this.setBoardSpace}">
+        <label for="columns">Number of columns</label>
+        <input id="columns" type="number" placeholder="Insert a number for columns" name="columns" value="0" @input="${this.setBoardSpace}">
+        <button @click="${this.loadReloadBoard}">${this.buttonMessage}</button>
+        <div id="control-pad">
+          <div id="up-section">
+            <div id="arrow-up" value="top" @click="${this._moveCurrentNode}"></div>
+          </div>
+          <div id="middle-section">
+            <div id="arrow-left" value="left" @click="${this._moveCurrentNode}"></div>
+            <div id="circle-container">
+              <div id="circle"></div>
+            </div>
+            <div id="arrow-right" value="right" @click="${this._moveCurrentNode}"></div>
+          </div>
+          <div id="bottom-section">
+          <div id="arrow-down" value="bottom" @click="down${this._moveCurrentNode}"></div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    `;
   }
 }
 customElements.define('tasksistant-board-case', TasksistantBoardCase);
